8b5df466623ef2051dc4139c19b14163fba59af7
Alvin <zyzzz0928@gmail.com>
2024-04-24 01:04:19 +0800

    local keymap about lazyterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,11 @@
 vim.keymap.set({ "n", "v" }, "<leader>x2", ":split<CR>", {})
 vim.keymap.set({ "n", "v" }, "<leader>x3", ":vsplit<CR>", {})
 vim.keymap.set({ "n", "v" }, "<leader>x1", "<C-w>o", {})
+vim.keymap.set({ "n", "v", "t" }, "<leader>w", ":w<CR>", {})
+
+-- config  for lazyterm
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+vim.keymap.set("n", "<leader>xz", lazyterm, {})
+vim.keymap.set("t", "<leader>xz", "<cmd>close<cr>", {})
